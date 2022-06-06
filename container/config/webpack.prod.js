559198ded013920e671e6380a6de4b7811443ba8
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-06-06 09:59:18 -0300

    config

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -9,7 +9,7 @@ const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js', // hash for cache issues
-    publicPath: '/container/latest/'
+    publicPath: '/container/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
